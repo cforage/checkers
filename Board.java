4fde02819ec8f20264a9d3c28dcb9e68dd20c4c5
Fint Rossmann Halpenny <rossmanf@tcd.ie>
2014-03-24 20:00:06 +0000

    Board outline

---
+++ Board.java
@@ -0,0 +1,20 @@
+public class Board
+{
+	private static int board[][];
+	private final BOARD_WIDTH = 8;
+	private final BOARD_HEIGHT = 8;
+	private final int RED = 1;
+	private final int BLACK = -1;
+	private final int EMPTY = 0;
+
+	public Board()
+	{
+		for(int i=0; i<BOARD_HEIGHT; i++)
+		{
+			for(int j=0; j<BOARD_WIDTH; j++)
+			{
+				
+			}
+		}
+	}
+}
