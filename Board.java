4504512d5c2d3649d734f797e501817d8fc1ddd1
cforage <colin.forage@gmail.com>
2014-04-19 16:12:54 -0500

    Better structure for how I think the Board class should work

--- Board.java
+++ Board.java
@@ -14,14 +14,24 @@ public class Board
 		{
 			for(int j=0; j<BOARD_WIDTH; j++)
 			{
-				
+				Tile t = new Tile(i,j);	
 			}
 		}
+	}
+	public boolean isTileEmpty(Tile tile){
+		//see if tile.status is empty
+		if(tile.status == 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+
 	}
 
 	public void movePiece(int piece, int row, int column, int new_row, int new_column)
 	{
-		if(validMove(row, column, new_row,new_column))
+		if(isValidMove(row, column, new_row,new_column))
 		{
 			board[new_row][new_column] = piece;
 		}else{
@@ -29,8 +39,25 @@ public class Board
 		}
 	}
 
-	public boolean validMove(int row, int column, int new_row, int new_column)
+	public boolean isValidMove(int row, int column, int new_row, int new_column)
 	{
+		
+		//	1.) check to see if isJumpAvailable == true. 
+		if(isJumpAvailable){
+			//force player to take jump.
+			//pop up an alert("There is a jump on the board at " + jumpLocation(tile) +" and you must take it");
+
+			//something to consider down the line, if there move the player is considering taking is available/unavailable, highlight the border of the tile to green/red
+			System.out.println("A jump is available and you must take it.");
+			return null;
+		}
+		// 2.) is the tile you're 
+		else if({
+
+		}
+
+		//not exactly sure this is the best way to try and check if this is a valid move. I think we need more conditions to check here.
+		/*
 		if(row+1 == new_row && column+1 == new_column && new_row < BOARD_WIDTH && new_column < BOARD_HEIGHT)
 		{
 			return true;
@@ -39,6 +66,7 @@ public class Board
 		}else{
 			return false;
 		}
+		*/
 	}
 
 
